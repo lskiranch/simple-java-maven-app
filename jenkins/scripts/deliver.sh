356699a02ccb1fa6280d45ca780f643e9e18a7be
L S Kiran CH <lskiranch@gmail.com>
2023-10-22 10:44:28 +0530

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -5,6 +5,7 @@ echo 'into the local Maven repository, which will ultimately be stored in'
 echo 'Jenkins''s local Maven repository (and the "maven-repository" Docker data'
 echo 'volume).'
 echo 'adding extra line'
+echo 'to check the jenkins detection'
 set -x
 mvn jar:jar install:install help:evaluate -Dexpression=project.name
 set +x
