cda1061b2c2d4d5032bcd29a9b098895348cd3f0
L S Kiran CH <lskiranch@gmail.com>
2023-10-22 06:53:01 +0530

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -4,6 +4,7 @@ echo 'The following Maven command installs your Maven-built Java application'
 echo 'into the local Maven repository, which will ultimately be stored in'
 echo 'Jenkins''s local Maven repository (and the "maven-repository" Docker data'
 echo 'volume).'
+echo 'adding extra line'
 set -x
 mvn jar:jar install:install help:evaluate -Dexpression=project.name
 set +x
